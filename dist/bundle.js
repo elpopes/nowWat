cfbc42758df154176643fb0dd1215980b21be6fd
lorenzo <lorenzotijerina@gmail.com>
2023-07-06 13:28:18 -0400

    insatll dependecies for babel

---
+++ dist/bundle.js
@@ -0,0 +1,96 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./cards.js":
+/*!******************!*\
+  !*** ./cards.js ***!
+  \******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nvar cards = [{\n  task: \"Clean your room\",\n  time: 15,\n  star: true\n}, {\n  task: \"Watch one show of parent's choice\",\n  time: 30,\n  star: false\n}, {\n  task: \"Read a chapter of your favorite book\",\n  time: 20,\n  star: true\n}, {\n  task: \"Play a math game online\",\n  time: 15,\n  star: true\n}, {\n  task: \"Draw a picture of your favorite animal\",\n  time: 30,\n  star: true\n}, {\n  task: \"Help mom or dad with what they're doing\",\n  time: 20,\n  star: true\n}, {\n  task: \"Spend time practicing a sport of your choice\",\n  time: 20,\n  star: true\n}, {\n  task: \"Write a letter to a family member\",\n  time: 15,\n  star: true\n}, {\n  task: \"Do stretching exercises\",\n  time: 10,\n  star: true\n}, {\n  task: \"Help fold laundry\",\n  time: 15,\n  star: true\n}, {\n  task: \"Practice writing cursive\",\n  time: 15,\n  star: true\n}, {\n  task: \"Brush your teeth and floss\",\n  time: 10,\n  star: true\n}, {\n  task: \"Play a board game with a family member\",\n  time: 20,\n  star: true\n}, {\n  task: \"Spend time learning a new language on a learning app\",\n  time: 15,\n  star: true\n}, {\n  task: \"Do a meditation\",\n  time: 10,\n  star: true\n}, {\n  task: \"Go outside and play\",\n  time: 20,\n  star: true\n}, {\n  task: \"Help dad or mom wash the dishes\",\n  time: 15,\n  star: true\n}, {\n  task: \"Write a short story\",\n  time: 20,\n  star: true\n}, {\n  task: \"Make your bed\",\n  time: 10,\n  star: true\n}, {\n  task: \"Listen to an educational podcast\",\n  time: 20,\n  star: true\n}, {\n  task: \"Practice a musical instrument\",\n  time: 20,\n  star: true\n}, {\n  task: \"Do a puzzle\",\n  time: 15,\n  star: true\n}, {\n  task: \"Plant a seed in the garden\",\n  time: 15,\n  star: true\n}, {\n  task: \"Do science experiments\",\n  time: 15,\n  star: true\n}, {\n  task: \"Feed your pet\",\n  time: 10,\n  star: true\n}, {\n  task: \"Practice multiplication tables\",\n  time: 15,\n  star: true\n}, {\n  task: \"Write three things you're grateful for today\",\n  time: 10,\n  star: true\n}, {\n  task: \"Help set the table for dinner\",\n  time: 10,\n  star: true\n}, {\n  task: \"Spend time learning about a historical event\",\n  time: 15,\n  star: true\n}, {\n  task: \"Organize your toys\",\n  time: 15,\n  star: true\n}, {\n  task: \"Do jumping jacks\",\n  time: 10,\n  star: true\n}, {\n  task: \"Write and perform a small play\",\n  time: 20,\n  star: true\n}, {\n  task: \"Play a memory card game\",\n  time: 15,\n  star: true\n}, {\n  task: \"Help dad or mom vacuum the house\",\n  time: 15,\n  star: true\n}, {\n  task: \"Make a DIY craft\",\n  time: 20,\n  star: true\n}, {\n  task: \"Draw a map of your neighborhood\",\n  time: 20,\n  star: true\n}, {\n  task: \"Water the plants\",\n  time: 10,\n  star: true\n}, {\n  task: \"Read a science article\",\n  time: 20,\n  star: true\n}, {\n  task: \"Spend time doing a painting\",\n  time: 15,\n  star: true\n}, {\n  task: \"Learn a new magic trick\",\n  time: 20,\n  star: true\n}, {\n  task: \"Have a dance party in your room\",\n  time: 20,\n  star: true\n}, {\n  task: \"Help make a grocery list\",\n  time: 15,\n  star: true\n}, {\n  task: \"Build a fort out of blankets\",\n  time: 30,\n  star: true\n}, {\n  task: \"Practice spelling words\",\n  time: 15,\n  star: true\n}, {\n  task: \"Learn to sew a button\",\n  time: 20,\n  star: true\n}, {\n  task: \"Spend time on a nature walk\",\n  time: 20,\n  star: true\n}, {\n  task: \"Do a crossword puzzle\",\n  time: 15,\n  star: true\n}, {\n  task: \"Help sort recycling\",\n  time: 10,\n  star: true\n}, {\n  task: \"Listen to a chapter of an audiobook\",\n  time: 20,\n  star: true\n}, {\n  task: \"Spend time learning about a planet of your choice\",\n  time: 15,\n  star: true\n}, {\n  task: \"Bake cookies with mom or dad\",\n  time: 30,\n  star: true\n}, {\n  task: \"Write a poem about your favorite thing\",\n  time: 20,\n  star: true\n}, {\n  task: \"Play a video game\",\n  time: 15,\n  star: false\n}, {\n  task: \"Do yoga for kids\",\n  time: 10,\n  star: true\n}, {\n  task: \"Learn a new dance routine\",\n  time: 20,\n  star: true\n}, {\n  task: \"Help prepare your lunch for tomorrow\",\n  time: 15,\n  star: true\n}];\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (cards);\n\n//# sourceURL=webpack:///./cards.js?");
+
+/***/ }),
+
+/***/ "./script.js":
+/*!*******************!*\
+  !*** ./script.js ***!
+  \*******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _cards_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./cards.js */ \"./cards.js\");\n\nfunction shuffle(array) {\n  var currentIndex = array.length;\n  var temporaryValue, randomIndex;\n  while (currentIndex !== 0) {\n    randomIndex = Math.floor(Math.random() * currentIndex);\n    currentIndex--;\n    temporaryValue = array[currentIndex];\n    array[currentIndex] = array[randomIndex];\n    array[randomIndex] = temporaryValue;\n  }\n  return array;\n}\nshuffle(_cards_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"]);\nvar deck = document.getElementById(\"deck\");\nvar startButton = document.getElementById(\"startButton\");\nvar timer = document.getElementById(\"timer\");\nvar currentCardIndex = 0;\nvar currentCard;\nvar timerInterval;\nfunction flipCard() {\n  if (currentCardIndex >= _cards_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"].length) {\n    alert(\"Congratulations! You have completed all the tasks.\");\n    return;\n  }\n  currentCard = _cards_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"][currentCardIndex];\n  deck.innerHTML = currentCard.task + \"<br/>\" + currentCard.time + \" min\";\n  deck.classList.add(\"flipped\");\n  startButton.style.backgroundColor = \"green\";\n  startTimer();\n}\nfunction startTimer() {\n  timerInterval = setInterval(countDown, 1000);\n  startButton.removeEventListener(\"click\", startTimer);\n}\nfunction countDown() {\n  var minutes = Math.floor(currentCard.time / 60); // Calculate minutes\n  var seconds = currentCard.time % 60; // Calculate remaining seconds\n\n  // Format the time display\n  var timeDisplay = minutes.toString().padStart(2, \"0\") + \":\" + seconds.toString().padStart(2, \"0\");\n  timer.textContent = timeDisplay;\n  if (currentCard.time <= 0) {\n    clearInterval(timerInterval);\n    timer.textContent = \"Time's up!\";\n    deck.classList.remove(\"flipped\");\n    startButton.addEventListener(\"click\", startTimer);\n    if (currentCard.star) {\n      var answer = confirm(\"Did you do your best?\");\n      if (answer) {\n        // User did their best, award a star\n        alert(\"Congratulations! You earned a star!\");\n      } else {\n        // User did not do their best\n        var tryAgain = confirm(\"Always try your best! Would you like to try again?\");\n        if (tryAgain) {\n          // Restart the game\n          currentCardIndex = 0;\n          flipCard();\n        }\n      }\n    }\n\n    // Move to the next card\n    currentCardIndex++;\n  }\n\n  // Decrement the time by 1 minute\n  currentCard.time -= 60;\n}\ndeck.addEventListener(\"click\", flipCard);\nstartButton.addEventListener(\"click\", startTimer);\n\n//# sourceURL=webpack:///./script.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./script.js");
+/******/ 	
+/******/ })()
+;
